264c2ddd6b797ffd8e0ad1032d968cc403a1bce4
zhouwenzhen <1023572695@qq.com>
2021-03-25 16:45:25 +0800

    把上次的脚本修正

--- src/test/java/testcase/SearchTest.java
+++ src/test/java/testcase/SearchTest.java
@@ -58,7 +58,7 @@ class SearchTest extends WebUIBase {
          WebSearchTask.wait(5);
 
          // 验证搜索结果
-         WebSearchTask.assertSearchResult(driver,"花朵");
+         WebSearchTask.assertSearchResult(driver,keyword);
 
 
 
